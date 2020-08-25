8a39489c46d9ca8485b16f5f0562e6d0a487c52c
eldinsmakic <eldinsmakic@hotmail.fr>
2020-08-25 23:36:31 +0200

    Initial Commit

---
+++ Marvelder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvelder
+//
+//  Created by eldin smakic on 25/08/2020.
+//  Copyright © 2020 eldin smakic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
