2c74937c369a990c0db443377b7346526df66259
Eldin <eldinsmakic@hotmail.fr>
2021-05-17 22:05:08 +0200

    [Feat]: Add StrechyHeaderImage

--- Marvelder/Components/AsyncImage.swift
+++ Marvelder/Components/AsyncImage.swift
@@ -47,3 +47,61 @@ struct AsyncImage<Placeholder: View>: View {
         }
     }
 }
+
+struct AsyncImageHeader<Placeholder: View>: View {
+
+    @ObservedObject private var loader: ImageLoader
+
+    @State var time = Timer.publish(every: 0.1, on: .current, in: .tracking).autoconnect()
+    @State var show = false
+
+    private let placeholder: Placeholder?
+
+    init(url: URL?, placeholder: Placeholder? = nil) {
+        if let url = url {
+            loader = ImageLoader(url: url)
+        } else {
+            let marvelImage = MarvelImage(
+                path: "http://i.annihil.us/u/prod/marvel/i/mg/9/80/59b3104f67eaf",
+                ext: "jpg"
+            )
+
+            loader = ImageLoader(url: marvelImage.url)
+        }
+
+        self.placeholder = placeholder
+    }
+
+    var body: some View {
+        image
+            .onAppear(perform: loader.load)
+            .onDisappear(perform: loader.cancel)
+    }
+
+    private var image: some View {
+        Group {
+            if loader.image != nil {
+                GeometryReader { geo in
+                    Image(uiImage: loader.image!)
+                    .resizable()
+                    .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+                    .frame(height: geo.frame(in: .global).minY > 0 ? UIScreen.main.bounds.size.height / 2.2 + geo.frame(in: .global).minY : UIScreen.main.bounds.size.height / 2.2, alignment: .center)
+                    .onReceive(self.time, perform: { _ in
+                        let y = geo.frame(in: .global).minY
+
+                        if -y > UIScreen.main.bounds.size.height / 2.2 - 50 {
+                            self.show = true
+                        } else {
+                            self.show = false
+                        }
+                    })
+                }.frame(height: UIScreen.main.bounds.size.height / 2.2)
+            } else {
+                placeholder
+            }
+        }
+    }
+}
+
+
+
