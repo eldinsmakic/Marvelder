17e7ffa21145e8170bfa214896fba5f0e327a818
Eldin <eldinsmakic@hotmail.fr>
2021-05-17 09:53:26 +0200

    [Feat](FakeData): Add better spacing, and stories type

--- Marvelder/Data/FakeData.swift
+++ Marvelder/Data/FakeData.swift
@@ -9,7 +9,9 @@
 enum Fake {}
 
 extension Fake {
+
     enum Character {
+
         static let character = MarvelCharacter(
             id: 5,
             name: "SpiderMan",
@@ -17,7 +19,9 @@ extension Fake {
             thumbnail: MarvelImage(
                 path: "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b",
                 ext: "jpg"
-            ), comics: Fake.Comic.comics
+            ),
+            comics: Fake.Comic.comics,
+            stories: Fake.Story.storys
         )
     }
 }
@@ -26,24 +30,59 @@ extension Fake {
 
     enum Comic {
 
-        static let comics = Comics(
+        static let comics = ComicsList(
             available: 10,
             items: [
-            ComicsRessourceItem(
-                resourceURI: "http://gateway.marvel.com/v1/public/comics/62304",
-                name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1"
-            ),
-            ComicsRessourceItem(
-                resourceURI: "http://gateway.marvel.com/v1/public/comics/78503",
-                name: "2099 Alpha (2019) #1"
-            ),
-            ComicsRessourceItem(
-                resourceURI: "http://gateway.marvel.com/v1/public/comics/60151",
-                name: "A YEAR OF MARVELS TPB (Trade Paperback)"
-            )
-         ])
+                MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/62304",
+                    name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1"
+                ),
+                    MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/78503",
+                    name: "2099 Alpha (2019) #1"
+                ),
+                    MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/60151",
+                    name: "A YEAR OF MARVELS TPB (Trade Paperback)"
+                )
+         ]
+        )
 
         static let marvelComicCell = MarvelComicCell(
+            id: 4,
+            title: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1",
+            description: "Spider-Man's Clone Saga has spun completely out of control! There's only one team that can save the wall-crawler now: the Marvel editors! Bob Harras, Ralph Macchio, Tom Brevoort, and more put their heads together to see if ANYTHING can get Spidey out of this doppelganger debacle!",
+            thumbnail: MarvelImage(
+                path: "http://i.annihil.us/u/prod/marvel/i/mg/9/80/59b3104f67eaf",
+                ext: "jpg")
+            )
+    }
+}
+
+
+extension Fake {
+
+    enum Story {
+
+        static let storys = StoryList(
+            available: 10,
+            items: [
+                MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/62304",
+                    name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1"
+                ),
+                    MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/78503",
+                    name: "2099 Alpha (2019) #1"
+                ),
+                    MarvelRessourceItem(
+                    resourceURI: "http://gateway.marvel.com/v1/public/comics/60151",
+                    name: "A YEAR OF MARVELS TPB (Trade Paperback)"
+                )
+            ]
+        )
+
+        static let storyCell = MarvelStoryCell(
             id: 4,
             title: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1",
             description: "Spider-Man's Clone Saga has spun completely out of control! There's only one team that can save the wall-crawler now: the Marvel editors! Bob Harras, Ralph Macchio, Tom Brevoort, and more put their heads together to see if ANYTHING can get Spidey out of this doppelganger debacle!",
